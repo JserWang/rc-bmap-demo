44093d8add2ce23132152fd88964e189ccb691ec
laoxuyo <1055006487@qq.com>
2018-09-10 23:45:16 +0800

    add split and copy code

--- src/App.js
+++ src/App.js
@@ -10,13 +10,82 @@ const {
 
 const { SubMenu } = Menu;
 
+const MIN_MENU_WIDTH = 200;// 菜单最小宽度
+const MAX_MENU_WIDTH = 400;// 菜单最大宽度
+let menuWidth = 200;
+const handleMouseUp = () => {
+  document.onmousemove = null;
+  document.onmouseup = null;
+};
+
+const handleMouseDown = (e) => {
+  const menu = document.getElementById('menu');
+  const right = document.getElementById('right');
+  const menuCurrentwidth = menu.clientWidth;// 菜单宽度
+  const currentClientX = e.clientX || window.event.clientX;// 记录x坐标
+  document.onmousemove = () => {
+    const clientX = e.clientX || window.event.clientX;
+    const dragWidth = clientX - currentClientX;// 拖拽距离
+    menuWidth = menuCurrentwidth + dragWidth;
+    if (menuWidth < MIN_MENU_WIDTH) {
+      menuWidth = MIN_MENU_WIDTH;
+      document.onmousemove = null;
+      document.onmouseup = null;
+    }
+    if (menuWidth > MAX_MENU_WIDTH) {
+      menuWidth = MAX_MENU_WIDTH;
+      document.onmousemove = null;
+      document.onmouseup = null;
+    }
+    menu.style.width = `${menuWidth}px`;
+    right.style.marginLeft = `${menuWidth}px`;
+  };
+};
+
+const toggleMenu = () => {
+  const menu = document.getElementById('menu');
+  const right = document.getElementById('right');
+  const toggleBtn = document.getElementById('toggleBtn');
+  // menu.style.width = menu.clientWidth === 0 ? menuWidth : 0;
+  if (menu.clientWidth === 0) {
+    menu.style.width = menuWidth;
+    right.style.marginLeft = `${menuWidth}px`;
+    toggleBtn.style.left = `${menuWidth}px`;
+  } else {
+    menu.style.width = 0;
+    right.style.marginLeft = 0;
+    toggleBtn.style.left = 0;
+  }
+};
+
+/*eslint-disable*/
 const App = () => (
   <Router basename="/examples">
     <Layout>
-      <Sider style={{
-        overflow: 'auto', height: '100vh', position: 'fixed', left: 0,
-      }}
+      <Sider
+        id="menu"
+        style={{
+          overflow: 'auto',
+          height: '100vh',
+          position: 'fixed',
+          left: 0,
+          maxWidth: '400px',
+          minWidth: '200px',
+        }}
       >
+        <div
+          style={{
+            position: 'absolute',
+            right: '0',
+            height: '100%',
+            width: '10px',
+            cursor: 'col-resize',
+            background: 'red',
+            zIndex: '99',
+          }}
+          onMouseDown={handleMouseDown}
+          onMouseUp={handleMouseUp}
+        />
         <Menu theme="dark" mode="inline">
           {
             menuData.map((item, index) => (
@@ -38,13 +107,27 @@ const App = () => (
           }
         </Menu>
       </Sider>
-      <Layout style={{ marginLeft: 200, overflow: 'hidden', height: '100vh' }}>
+      <Layout id="right" style={{ marginLeft: 200, overflow: 'hidden', height: '100vh' }}>
         <Content>
+        <div
+          id="toggleBtn"
+          style={{
+            position: 'absolute',
+            left: '200px',
+            height: '40px',
+            width: '10px',
+            background: 'red',
+            top: '50%',
+            marginTop: '-20px',
+            zIndex: '99999',
+          }}
+          onClick={toggleMenu}
+        />
           { route() }
         </Content>
       </Layout>
     </Layout>
   </Router>
 );
-
+  /*eslint-disable*/
 export default App;
--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -18,9 +18,14 @@ const babelOptions = {
   ],
 };
 
+const MIN_CODE_WIDTH = 0;// 代码编辑器最小宽度
+const MIN_MAP_WIDTH = 0;// 地图最小宽度
+
 class Container extends Component {
   componentDidMount() {
     this.handleRunClick();
+    this.codeDiv = document.getElementById('codeDiv');
+    this.mapDiv = document.getElementById('mapDiv');
   }
 
   handleCodeChange = (code) => {
@@ -49,20 +54,66 @@ class Container extends Component {
     }
   }
 
+  copyCode = () => {
+    const { code } = this.props;
+    const input = document.createElement('input');
+    input.setAttribute('readonly', 'readonly');
+    input.setAttribute('value', (this.currentCode || code));
+    document.body.appendChild(input);
+    input.select();
+    if (document.execCommand('copy')) {
+      document.execCommand('copy');
+      alert('复制成功');
+    }
+    document.body.removeChild(input);
+  }
+
   transformCode = (code) => {
     const result = window.Babel.transform(code, babelOptions);
     return result.code;
+  }
+
+  handleMouseDown = (e) => {
+    const codeCurrentwidth = this.codeDiv.clientWidth;// 编辑器宽度
+    const mapCurrentwidth = this.mapDiv.clientWidth;// 地图宽度
+    const currentClientX = e.clientX || window.event.clientX;// 记录x坐标
+    document.onmousemove = () => {
+      const clientX = e.clientX || window.event.clientX;
+      const dragWidth = clientX - currentClientX;// 拖拽距离
+      let codeWidth = codeCurrentwidth + dragWidth;
+      let mapWidth = mapCurrentwidth - dragWidth;
+      if (codeWidth < MIN_CODE_WIDTH) {
+        codeWidth = MIN_CODE_WIDTH;
+        mapWidth = mapCurrentwidth + codeCurrentwidth - codeWidth;
+        document.onmousemove = null;
+        document.onmouseup = null;
+      }
+      if (mapWidth < MIN_MAP_WIDTH) {
+        mapWidth = MIN_MAP_WIDTH;
+        codeWidth = codeCurrentwidth + mapCurrentwidth - mapWidth;
+        document.onmousemove = null;
+        document.onmouseup = null;
+      }
+      this.mapDiv.style.width = `${mapWidth}px`;
+      this.codeDiv.style.width = `${codeWidth}px`;
+    };
+  }
+
+  handleMouseUp = () => {
+    this.handleRunClick();
+    document.onmousemove = null;
+    document.onmouseup = null;
   }
 
   render() {
     const { code } = this.props;
     return (
       <Row style={{ height: '100vh' }}>
-        <Col span={7} style={{ background: 'rgb(245, 242, 240)', height: '100vh' }}>
+        <Col span={7} id="codeDiv" style={{ background: 'rgb(245, 242, 240)', height: '100vh' }}>
           <div className={styles.codeHeader}>
             <span>源代码编辑器</span>
             <span>
-              <Button shape="circle" icon="copy" />
+              <Button shape="circle" icon="copy" onClick={this.copyCode} />
               <Button
                 type="primary"
                 className={styles.button}
@@ -79,7 +130,20 @@ class Container extends Component {
             onChange={this.handleCodeChange}
           />
         </Col>
-        <Col span={17} style={{ height: '100vh' }}>
+        <Col id="mapDiv" span={17} style={{ height: '100vh' }}>
+          <div
+            style={{
+              position: 'absolute',
+              left: '-3px',
+              height: '100%',
+              width: '10px',
+              cursor: 'col-resize',
+              background: 'red',
+              zIndex: '99',
+            }}
+            onMouseDown={this.handleMouseDown}
+            onMouseUp={this.handleMouseUp}
+          />
           <div id="demo" />
         </Col>
       </Row>
