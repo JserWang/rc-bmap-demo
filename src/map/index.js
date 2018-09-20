5b14ec86bec3922811874d5dcf34f5b1a1656680
JserWang <jserwang@gmail.com>
2018-09-20 13:39:50 +0800

    refactor: router usage

--- src/map/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Container from 'components/Container';
-import MapCode from './index.md';
-
-function Example() {
-  return (<Container code={MapCode} />);
-}
-
-export default Example;
